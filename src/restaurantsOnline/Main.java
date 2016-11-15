52d3c2b3d75e4fe06a67cf0eaeafd963d8b01c5d
zina99 <msn_zina@hotmail.com>
2016-11-15 15:02:31 +0100

    clase main
    primero parte del main

--- src/restaurantsOnline/Main.java
+++ src/restaurantsOnline/Main.java
@@ -1,8 +1,104 @@
 package restaurantsOnline;
 
+import java.util.Scanner;
 public class Main {
 
-	public static void main(String[] args) {
+	    static Scanner teclat = new Scanner(System.in);
+	    
+	    public static void mostraMenu()
+	    {
+	    	System.out.println("******RESTAURANTE ON LINE*******");
+	        System.out.println("\n\nOpcions del menu:");
+	        System.out.println("\n\t1. Afegir nous productes");
+	        System.out.println("\n\t2.  Eliminar producte");    
+	        System.out.println("\t3. Carregar els productes a partir d'un fitxer de dad es");
+	        System.out.println("\t4. Consultar la informacio que es demana al registre ");
+	        System.out.println("\t5. Crear un nou cliente "); 
+	        System.out.println("\t6. Llistar  totes les comandes que ha fet un clint,ordenades de mes nova a mes antiga ");
+	        System.out.println("\t7. Obrir una nova comanda per a un client ");
+	        System.out.println("\t8. Sortir");
+	        System.out.print("\n\t\t\tIndica opcio:\n");
+	    }
+	    public static void afegirProducte()
+	    {
+	      
+	    }
+	    public static void eliminarProducte()
+	    {
+	      
+	    }
+	    public static void carregarProducte()
+	    {
+	        
+	    }
+
+	    public static void consultarInformacio()
+	    {
+	    }
+
+	    public static void crearClient()
+	    {
+	    }
+	    
+
+	    private static void llistarComandes()
+	    {
+
+	    }
+
+	    private static void novaComanda()
+	    
+	    {
+	    	
+	    }
+
+	 
+
+	    public static void main(String[] args)
+	    {
+	        int opcio;
+	    
+
+	        mostraMenu();
+	        opcio = Integer.parseInt(teclat.nextLine());
+	        while(opcio != 6)
+	        {
+	         
+	            switch(opcio)
+	            {
+	                case 1:
+	                	afegirProducte();
+	                    break;
+	                case 2:
+	                	eliminarProducte();
+	                    break;
+	                case 3:
+	                	carregarProducte();
+	                    break;
+	                case 4:
+	                	consultarInformacio();
+	                    break;
+	                case 5:
+	                	crearClient();
+	                    break;
+	                case 6: 
+	                	llistarComandes();
+	                	break;
+	                case 7:
+	                	novaComanda();
+	                    break;
+	               
+
+	            }
+	            mostraMenu();
+	            opcio = Integer.parseInt(teclat.nextLine());
+	        }
+	    }
+	
+}
+
+
+	/*public static void main(String[] args) {
 		Beguda b1 = new Beguda("cola",2,10,30,false);
 		Beguda b2 = new Beguda("fanta",1.5,20,30,false);
 		Plat p1 = new Plat("Amanida", 5, 20);
@@ -30,4 +126,4 @@ public class Main {
 
 	}
 
-}
+}*/
